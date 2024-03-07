8674d7762d743324b751ef04bf9fe6280926c24e
Stsama <albertezoua98@gmail.com>
2024-03-06 18:07:32 -0800

    task 3 fixing

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["52.91.202.237", "34.224.16.163"]
 
 
 def do_pack():
